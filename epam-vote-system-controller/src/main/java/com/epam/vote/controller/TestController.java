c6819ed8ac6b1a35532853fa30c9a25b01a2d9c7
Dzianis Zatsiupa <dencheek64@gmail.com>
2022-04-06 16:59:53 +0300

    Fix mistake in comments

--- epam-vote-system-controller/src/main/java/com/epam/vote/controller/TestController.java
+++ epam-vote-system-controller/src/main/java/com/epam/vote/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * "Show hello world message"
+ * "Shows hello world message"
  *
  * Copyright (C) 2021
  *
