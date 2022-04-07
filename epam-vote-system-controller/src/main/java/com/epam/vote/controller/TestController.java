7c5b494fdc945b041f43ad2b0f0ef27006c9c7a3
Dzianis Zatsiupa <dencheek64@gmail.com>
2022-04-07 10:44:53 +0300

    Fix comments

--- epam-vote-system-controller/src/main/java/com/epam/vote/controller/TestController.java
+++ epam-vote-system-controller/src/main/java/com/epam/vote/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * "Shows hello world message"
+ * Shows hello world message.
  *<p/>
  * Copyright (C) 2017 epam.com
  *<p/>
